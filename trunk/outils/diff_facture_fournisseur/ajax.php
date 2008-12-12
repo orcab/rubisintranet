819b7ddd0de4a36eff380efeb52537ec4ef800ea
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2008-12-12 09:20:43 +0000

    - diff_facture_fournisseur : Possibilité d'étider la différence de valeur et le commentaire - Commande fournisseur PDF : Rajout d'une phrase pour obliger les accusés de réception

--- trunk/outils/diff_facture_fournisseur/ajax.php
+++ trunk/outils/diff_facture_fournisseur/ajax.php
@@ -1,9 +1,12 @@
 <?
 include('../../inc/config.php');
-$loginor  = odbc_connect(LOGINOR_DSN,LOGINOR_USER,LOGINOR_PASS) or die("Impossible de se connecter à Loginor via ODBC ($LOGINOR_DSN)");
+
 
  ////// RECHERCHE S'IL Y A PLUSIEURS FOURNISSEUR POUR CETTE FACTURE
-if ($_GET['what'] == 'check_nb_fournisseur' && isset($_GET['no_fact']) && $_GET['no_fact']) {
+if (isset($_GET['what']) && $_GET['what'] == 'check_nb_fournisseur' &&
+	isset($_GET['no_fact']) && $_GET['no_fact']) {
+		$loginor  = odbc_connect(LOGINOR_DSN,LOGINOR_USER,LOGINOR_PASS) or die("Impossible de se connecter à Loginor via ODBC ($LOGINOR_DSN)");
+
 		$nofact_escape = trim(strtoupper(mysql_escape_string($_GET['no_fact'])));
 
 		// on regarde si on a plusieur fournisseur qui ont le meme n° de facture
@@ -22,6 +25,26 @@ EOT;
 		}
 		echo '['.join(',',$fournisseurs).']';
 		//print_r($fournisseurs);
+
+		odbc_close($loginor);
 }
 
+
+// sauevgarde des modifications manuelle des différences
+elseif (isset($_POST['what']) && $_POST['what'] == 'save_diff' &&
+		isset($_POST['id']) && $_POST['id'] &&
+		isset($_POST['diff']) && isset($_POST['com'])) {
+	
+		$mysql    = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS) or die("Impossible de se connecter");
+		$database = mysql_select_db(MYSQL_BASE) or die("Impossible de se choisir la base");
+
+		$id_escape	= trim(strtoupper(mysql_escape_string($_POST['id'])));
+		$diff_escape	= trim(strtoupper(mysql_escape_string($_POST['diff'])));
+		$com_escape		= trim(strtoupper(mysql_escape_string($_POST['com'])));
+
+		mysql_query("UPDATE diff_cde_fourn SET diff=$diff_escape, commentaire='$com_escape' WHERE id=$id_escape") ;
+		echo (mysql_affected_rows($mysql) == 1) ? '' : "Une erreur est survenu, impossible d'enregistrer les modifications ou aucune modification effectée";
+
+		mysql_close($mysql);
+}
 ?>
