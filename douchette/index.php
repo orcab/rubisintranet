10f61cd1e6c061b70f44099b6ff6a036c581e6ce
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2009-04-15 07:17:39 +0000

    - Ajout du programme de douchette (lien entre code et code barre)

---
+++ douchette/index.php
@@ -0,0 +1,39 @@
+<?
+include('../inc/config.php');
+?><html>
+<head>
+<title>Menu Douchette</title>
+<style>
+
+body,td{
+	font-family:verdana;
+	font-size:0.8em;
+}
+
+a img { 
+	border:none;
+}
+
+a {
+	text-decoration:none;
+}
+
+a:hover {
+	text-decoration:underline;
+}
+
+</style>
+
+</head>
+<body style="margin:0px;padding:0px;">
+<center>
+<table style="width:70%;text-align:center;border:solid 1px #AAA;">
+<tr>
+	<td></td>
+	<td><a href="add_gencode.php"><img src="gfx/add_gencode.gif"><br>Saisie de GENCODE</a></td>
+	<td></td>
+</tr>
+</table>
+</center>
+</body>
+</html>
