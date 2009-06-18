a46f0e116397d722276ab2ba16e3c2bd3ade77bf
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2009-06-18 14:46:02 +0000

    - js : MAJ de JQuery - Devis2 : continuation du dvdpt de l'interface n°2 - Cde fournisseur : Ajout des heure d'ouverture

--- devis2/creation_devis.php
+++ devis2/creation_devis.php
@@ -12,13 +12,15 @@ if (!($droit & PEUT_CREER_DEVIS)) { // n'a pas le droit de faire des devis
 
 
 // mode creation ou modification ?
-$id = isset($_GET['id']) ? $_GET['id'] : '' ;
+$id = isset($_GET['id']) ? mysql_escape_string($_GET['id']) : '' ;
 
 // recherche du l'entete du devis si en modification
 if($id) { // modif
-	$res_devis = mysql_query("SELECT *,DATE_FORMAT(`date`,'%d/%m/%Y') AS date_formater,DATE_FORMAT(`date`,'%H:%i') AS heure,DATE_FORMAT(date_maj,'%d/%m/%Y') AS date_maj_formater,CONCAT(DATE_FORMAT(`date`,'%b%y-'),id) AS numero FROM devis WHERE id=$id LIMIT 0,1") or die("Requete impossible ".mysql_error()) ;
+	$res_devis = mysql_query("SELECT *,DATE_FORMAT(`date`,'%d/%m/%Y') AS date_formater,DATE_FORMAT(`date`,'%H:%i') AS heure,DATE_FORMAT(date_maj,'%d/%m/%Y') AS date_maj_formater,CONCAT(DATE_FORMAT(`date`,'%b%y-'),id) AS numero FROM devis WHERE id='$id' LIMIT 0,1") or die("Requete impossible ".mysql_error()) ;
 	$row_devis = mysql_fetch_array($res_devis);
 }
+
+//print_r($row_devis);
 
 ?><html>
 <head>
@@ -40,12 +42,14 @@ function affiche_adherent() {
 		$('#artisan_nom_libre').show('fast');
 }
 
+function cache_sugest() {
+	$('div#sugest').hide('normal');
+}
+
 var tr ;
 var all_results = new Array();
 var nb_results_by_page = 20 ;
 var recherche = '';
-var nb_option = 1;
-
 
 
 function make_all_bind() {
@@ -56,47 +60,30 @@ function make_all_bind() {
 	$('input[type=text]').blur(function()	{	$(this).css('background','');	});
 	$('input[type=text]').focus(function()	{	$(this).css('background','#e7eef3');	});
 
-	// bind des checkbox "Commentaire ou pas"
-	$('input[name$=_com]').unbind('click');
-	$('input[name$=_com]').click(function(){
-		$(this).parent().parent().children('td').children('.ligne').attr('disabled', $(this).attr('checked') ? 'disabled':'');
-	});
-
-	// bind des checkbox Options
-	$('input[name$=a_opt]').unbind('click');
-	$('input[name$=a_opt]').click(function(){
+	// click sur options
+	$('input[name=checkbox]').unbind('click');
+	$('input[type=checkbox]').click(function() {
+		$(this).parent('td').parent('tr').children('td').children('input[name^=a_hid_opt]').val( $(this).attr('checked') ? '1' : '0'  );
 		recalcul_total();
 	});
 
 	// ajoute un ligne au dessus de la ligne courante
-	$('img[name$=_add]').unbind('click');
-	$('img[name$=_add]').click(function() {
+	$('img[name^=a_add]').unbind('click');
+	$('img[name^=a_add]').click(function() {
 		$(this).parent().parent().before( pattern_ligne );
 		make_all_bind();
 	});
 
 	// supprime une ligne du tableau en cliquant sur l'image
-	$('img[name$=_del]').unbind('click');
-	$('img[name$=_del]').click(function() {
+	$('img[name^=a_del]').unbind('click');
+	$('img[name^=a_del]').click(function() {
 		$(this).parent().parent().remove();  // supprime le TR
 	});
 
 
-	// calcul le total de la ligne
-	$('input[name=a_qte], input[name=a_pu]').unbind('keyup');
-	$('input[name=a_qte], input[name=a_pu]').keyup(function() {
-		recalcul_total();
-					
-/*		alert($(this).parents('tr').children('td').children('input[name=a_pu]').map(function () { 
-                                return this.tagName; 
-                              }).get().join(", "));
-*/
-		//alert('toto');
-	});
-
 	// on doit aller chercher les infos dans la BD et les ramener sur la page
-	$('input[name=a_reference]').unbind('keyup');
-	$('input[name=a_reference]').keyup(function() {
+	$('input[name^=a_reference]').unbind('keyup');
+	$('input[name^=a_reference]').keyup(function() {
 		tr = $(this).parents().parents('tr');
 		recherche = $(this).val();
 		var div_offset = $(this).offset();
@@ -131,10 +118,12 @@ function draw_page(pageno) {
 	
 	for(i=nb_results_by_page * (pageno-1) ; i<all_results.length && i<nb_results_by_page * (pageno-1) + nb_results_by_page ; i++) {
 		div.append(	'<tr onclick="insert_ligne(\''+all_results[i].id+'\');">' + 
-									'<td style="padding-right:10px;">' + all_results[i].reference.replace(recherche,'<strong>'+recherche+'</strong>') + '<td>' +
-									'<td style="color:green;padding-right:10px;">' + all_results[i].fournisseur + '<td>' +
-									'<td style="padding-right:10px;">' + all_results[i].designation + '<td>' +
-									'<td style="font-weight:bold;">' + Math.round(all_results[i].px_public * 100)/100 + '&euro;<td>' +
+						'<td style="padding-right:10px;">' + all_results[i].reference.toUpperCase().replace(recherche.toUpperCase(),'<strong>'+recherche.toUpperCase()+'</strong>') + '<td>' +
+						'<td style="color:green;padding-right:10px;">'		+ all_results[i].fournisseur + '<td>' +
+						'<td style="padding-right:10px;width:500px;">'		+ all_results[i].designation +
+								(all_results[i].couleur ? '<br/>Couleur : '	+all_results[i].couleur:'') +
+								(all_results[i].taille ? '<br/>Taille : '	+all_results[i].taille:'') +'<td>' +
+						'<td style="font-weight:bold;">' + Math.round(all_results[i].prix * 100)/100 + '&euro;<td>' +
 					'</tr>'
 		); // on affiche les suggestions
 	}
@@ -145,46 +134,62 @@ function draw_page(pageno) {
 	div.append('Page '+pageno);
 	if (pageno < lastpage) div.append('&nbsp;&nbsp;&nbsp;&nbsp;<span class="navig"><a href="javascript:draw_page('+ parseInt(pageno+1) +');">suiv.&gt;</a>&nbsp;&nbsp;&nbsp;<a href="javascript:draw_page('+lastpage+');">&gt;&gt;</a></span>');
 
-	div.append('</td></tr></tfoot></table>');
+	div.append('<div style="float:right;"><a href="javascript:cache_sugest();">Fermer [X]</a></div></td></tr></tfoot></table>');
 }
 
 
 function insert_ligne(id) {
-	//alert(tr);
+	//alert(id);
 	$('div#sugest').hide(); // on cache la boite
 	$.getJSON('ajax.php', { what:'get_detail', val: id  } ,
 			function(data){
-				//alert(data.reference);
-				tr.children('td').children('input[name=a_reference]').val(data.reference);
-				tr.children('td').children('input[name=a_fournisseur]').val(data.fournisseur);
-				tr.children('td').children('textarea[name=a_designation]').val(data.designation);
-				tr.children('td').children('input[name=a_qte]').val(1);
-				tr.children('td').children('input[name=a_pu]').val((Math.round(data.px_public * 100)/100));
+				//alert(data.couleur);
+				var tmp = tr.children('td') ;
+				tmp.children('input[name^=a_reference]').val(data.reference);
+				tmp.children('input[name^=a_fournisseur]').val(data.fournisseur);
+				tmp.children('textarea[name^=a_designation]').val(
+								data.designation +
+								(data.couleur ? '\nCouleur : '+data.couleur:'') +
+								(data.taille ? '\nTaille : '+data.taille:''));
+				tmp.children('input[name^=a_qte]').val(1);
+
+				// pour le débug des prix.
+				tmp.children('input[name^=a_pu]').parents('td').html('<input type="text" name="a_pu[]" value="" class="pu" onkeyup="recalcul_total();"/>');
+				tmp.children('input[name^=a_pu]').val((Math.round(data.prix		* 100)/100)); // prix expo
+				tmp.children('input[name^=a_adh_pu]').val((Math.round(data.px_adh	* 100)/100)); // prix adh
+
+				// debug des différents prix calculé.
+				tmp.children('input[name^=a_pu]').after('<br/>coop '	+ Math.round(data.px_coop		* 100)/100 + 
+														'<br/>adh '		+ Math.round(data.px_adh		* 100)/100 + 
+														'<br/>expo '	+ Math.round(data.px_expo		* 100)/100 + 
+														'<br/>pub '		+ Math.round(data.px_public	* 100)/100
+														);
 				recalcul_total();
 			}
 	);
 	
 }
 
+
 function recalcul_total() {
 	total = 0;
 	option = 0;
-	$('input[name=a_qte]').each(function() {
-		var pu	= parseFloat($(this).parents('tr').children('td').children('input[name=a_pu]').val());
-		var qte = parseFloat($(this).parents('tr').children('td').children('input[name=a_qte]').val());
+	$('input[name^=a_qte]').each(function() {
+		var pu	= parseFloat($(this).parents('tr').children('td').children('input[name^=a_pu]').val());
+		var qte = parseFloat($(this).parents('tr').children('td').children('input[name^=a_qte]').val());
 		if (pu >= 0 && qte >=0) {
 			var val = (Math.round(qte * pu * 100)/100) ;
-			$(this).parents('tr').children('td[name=a_pt]').html(val + '&euro;');
+			$(this).parents('tr').children('td[name^=a_pt]').html(val + '&euro;');
 			// on vérifie si c'est une option
-			if ($(this).parents('tr').children('td').children('input[name=a_opt]').attr('checked')) // cas d'une option, on ne l'a compte pas dans le total
+			if ($(this).parents('tr').children('td').children('input[name^=a_opt]').attr('checked')) // cas d'une option, on ne l'a compte pas dans le total
 				option++;
 			else
 				total += val ;
 		} else
-			$(this).parents('tr').children('td[name=a_pt]').text('');
+			$(this).parents('tr').children('td[name^=a_pt]').text('');
 	});
 
-	$('span#total').text(total);
+	$('span#total').text(Math.round(total * 100)/100);
 	if (option > 0)
 		$('span#options').text("Le total ne tient pas compte des " + option + " option(s) choisit");
 	else
@@ -195,20 +200,19 @@ function recalcul_total() {
 <?
 
 $pattern_ligne = <<<EOT
-<tr id="a_ligne">
+<tr>
 	<td>
 		<img src="gfx/add.png" name="a_add" title="Ajoute une ligne au dessus" /><br/>
 		<img src="../gfx/delete_micro.gif" name="a_del" title="Supprime la ligne" />
+		<input type="hidden"	name="a_adh_pu[]"	value="0"/>
+		<input type="hidden"	name="a_hid_opt[]"	value="0"/>
 	</td>
-	<td class="opt">
-		<input type="checkbox" name="a_com" /><label for="a_com">Com.</label><br/>
-		<input type="checkbox" class="ligne" name="a_opt" /><label for="a_opt">Opt.</label>
-	</td>
-	<td><input type="text" name="a_reference"		class="ref ligne"			value="" /></td>
-	<td><input type="text" name="a_fournisseur"		class="fournisseur ligne"	value="" /></td>
-	<td><textarea name="a_designation"	rows="3"	class="designation"></textarea></td>
-	<td><input type="text" name="a_qte" value=""	class="qte ligne" /></td>
-	<td><input type="text" name="a_pu"	value=""	class="pu ligne" /></td>
+	<td class="opt"><input type="checkbox" name="a_opt[]" /><label for="a_opt">Opt.</label></td>
+	<td><input type="text"		name="a_reference[]"	value=""		class="ref"			 autocomplete="off" /></td>
+	<td><input type="text"		name="a_fournisseur[]"	value=""		class="fournisseur"	 /></td>
+	<td><textarea				name="a_designation[]"	rows="3"	class="designation"></textarea></td>
+	<td><input type="text"		name="a_qte[]"		value="0"	class="qte" onkeyup="recalcul_total();"/></td>
+	<td><input type="text"		name="a_pu[]"		value="0"	class="pu"  onkeyup="recalcul_total();"/></td>
 	<td name="a_pt"></td>
 </tr>
 EOT;
@@ -223,7 +227,8 @@ $(document).ready(function(){
 	
 	$('#add_dix_ligne').click(function() {
 		for(var i=0 ; i<=9 ; i++)
-			$('#add_ligne').click();
+			$('#lignes tbody').append( pattern_ligne );
+		make_all_bind();
 	});
 
 	// ajoute une ligne à la fin du tableau
@@ -234,8 +239,12 @@ $(document).ready(function(){
 	
 
 	// au chargement de la page, on ajoute une ligne au tableau des details
-	$('#add_ligne').click();
+<?	if (!$id) { // si aucune ligne sur le devis, on en propose une ?>
+		$('#add_ligne').click();
+<?	} ?>
 
+	make_all_bind();
+	recalcul_total();
 }); // fin on document ready
 
 </script>
@@ -338,13 +347,15 @@ span#options {
 	font-size:0.8em;
 }
 
+td.devis_id { font-weight:bold; }
+
 </style>
 
 <body>
 <div id="sugest"></div><!-- pour la sugestion des résultat ajax -->
 
 <form method="post" action="generation_devis_pdf.php" name="creation_devis">
-
+<input type="hidden" name="id_devis" value="<?=$id?>" />
 <div id="cadre-exterieur">
 
 
@@ -394,7 +405,7 @@ span#options {
 	<th>Date</th>
 	<td nowrap>
 		<input type="text" id="devis_date" name="devis_date" value="<?= $id ? $row_devis['date_formater'] : date('d/m/Y')?>" size="8">
-		<button id="trigger" style="background:url('../js/jscalendar/calendar.gif') no-repeat left top;border:none;cursor:pointer;) no-repeat left top;">&nbsp;</button>
+		<img src="../js/jscalendar/calendar.gif" id="trigger" style="vertical-align:middle;cursor: pointer;"title="Date selector" />
 		<script type="text/javascript">
 		  Calendar.setup(
 			{
@@ -423,7 +434,7 @@ span#options {
 	</td>
 </tr>
 <tr>
-	<td><?= $id ? "Devis N°$id" :'' ?></td>
+	<td class="devis_id"><?= $id ? "Devis N°$id" :'' ?></td>
 	<td>
 	</td>
 	<td></td>
@@ -461,7 +472,31 @@ span#options {
 		</tr>
 		</thead>
 		<tbody>
-			
+<?	if ($id) {
+		$res_detail = mysql_query("SELECT * FROM devis_ligne WHERE id_devis='$id'");
+		while($row_detail = mysql_fetch_array($res_detail)) {
+
+			$custom_ligne = $pattern_ligne;
+			if ($row_detail['option']) { // c'est d'une option
+				$custom_ligne = preg_replace('/\sname="a_opt\[\]"\s/i',' name="a_opt[]" checked="checked" ',$custom_ligne);
+				$custom_ligne = preg_replace('/\sname="a_hid_opt\[\]"\svalue="0"/i',' name="a_hid_opt[]" value="1" ',$custom_ligne);
+			}
+			if ($row_detail['ref_fournisseur'])
+				$custom_ligne = preg_replace('/\sname="a_reference\[\]"\s+value=""\s/i',' name="a_reference[]" value="'.$row_detail['ref_fournisseur'].'" ',$custom_ligne);
+			if ($row_detail['fournisseur'])
+				$custom_ligne = preg_replace('/\sname="a_fournisseur\[\]"\s+value=""\s/i',' name="a_fournisseur[]" value="'.$row_detail['fournisseur'].'" ',$custom_ligne);
+			if ($row_detail['designation'])
+				$custom_ligne = preg_replace('/\s+class="designation"\s*><\/textarea>/i',' class="designation">'.$row_detail['designation'].'</textarea>',$custom_ligne);
+			if ($row_detail['qte'])
+				$custom_ligne = preg_replace('/\sname="a_qte\[\]"\s+value="0"\s/i',' name="a_qte[]" value="'.$row_detail['qte'].'" ',$custom_ligne);
+			if ($row_detail['puht'])
+				$custom_ligne = preg_replace('/\sname="a_pu\[\]"\s+value="0"\s/i',' name="a_pu[]" value="'.$row_detail['puht'].'" ',$custom_ligne);
+			if ($row_detail['pu_adh_ht'])
+				$custom_ligne = preg_replace('/\sname="a_adh_pu\[\]"\s+value="0"\s/i',' name="a_adh_pu[]" value="'.$row_detail['pu_adh_ht'].'" ',$custom_ligne);
+
+			echo $custom_ligne;
+		}
+	} ?>
 		</tbody>
 		<tfoot>
 			<td style="text-align:left" colspan="8">
@@ -475,7 +510,7 @@ span#options {
 <fieldset class="total">
     <legend>Total :</legend>
 	<div id="div_bouton">
-		<input type="button" value="Générer le devis" class="button" style="background:#e7eef3;" />
+		<input type="submit" value="Générer le devis" class="button pdf" style="background-color:#e7eef3;" />
 		<input type="button" value="Générer le devis en prix ADH" style="border:none;background:none;color:white;" />
 	</div>
 	<div id="div_total">
