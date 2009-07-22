170b58f6dde803f8b3d4eec586747a274ad91c09
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2009-07-22 10:05:03 +0000

    - stats de salle expo : ok - ajout de l'outil pour prévenir les adh de la mise a dispo de leur matos

---
+++ trunk/devis2/graph_cde.php
@@ -0,0 +1,169 @@
+<?
+
+include('../inc/config.php');
+include('../inc/iCalParser/ical-parser-class.php');
+include('../inc/jpgraph/src/jpgraph.php');
+include('../inc/jpgraph/src/jpgraph_line.php');
+include('../inc/jpgraph/src/jpgraph_bar.php');
+
+$mysql    = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS) or die("Impossible de se connecter");
+$database = mysql_select_db(MYSQL_BASE) or die("Impossible de se choisir la base");
+
+$mois = array('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec');
+
+define('COMMANDE',0);
+define('MONTANT_COMMANDE',1);
+define('DEVIS',2);
+define('TAUX',3);
+
+//chargement des données
+// calcul du taux de devis/cmd de la salle
+$sql = <<<EOT
+SELECT  DISTINCT ( DATE_FORMAT( `date` , '%b %Y' )) AS date_formater,
+		DATE_FORMAT( `date` , '%Y%m' ) AS date_formater_ical,
+		DATE_FORMAT( `date` , '%Y' ) AS annee,
+        COUNT(id) AS nb_devis
+FROM  devis
+WHERE artisan<>'EDITION'
+GROUP BY date_formater
+ORDER BY `date` ASC
+EOT;
+
+$res = mysql_query($sql) or die("Ne peux pas trouver le nombre de devis ".mysql_error());
+
+$sql2 = <<<EOT
+SELECT  DISTINCT ( DATE_FORMAT( `date` , '%b %Y' )) AS date_formater,
+        COUNT(num_cmd_rubis) AS nb_cmd_rubis,
+		SUM(mtht_cmd_rubis) AS montant_cmd
+FROM devis
+WHERE     num_cmd_rubis NOT LIKE 'ANNULE'
+      AND num_cmd_rubis NOT LIKE 'SUSPENDU'
+      AND num_cmd_rubis IS NOT NULL
+      AND num_cmd_rubis <> ''
+	  AND artisan<>'EDITION'
+GROUP BY date_formater
+ORDER BY `date` ASC
+EOT;
+
+$res2 = mysql_query($sql2) or die("Ne peux pas trouver le nombre de cmd ".mysql_error());
+$cmd_rubis = array();
+while($row = mysql_fetch_array($res2)) {
+	$cmd_rubis[$row['date_formater']] = array($row['nb_cmd_rubis'],$row['montant_cmd']);
+}
+
+while($row = mysql_fetch_array($res)) {
+	if(isset($cmd_rubis[$row['date_formater']])) {
+		$cmd_rubis[$row['date_formater']][] = $row['nb_devis'];
+		
+	} else {
+		$cmd_rubis[$row['date_formater']] = array(0,0,$row['nb_devis'],0);
+	}
+}
+
+//print_r($cmd_rubis);
+
+// Create the Pie Graph. 
+$data_COMMANDE = array();
+foreach($cmd_rubis as $vals)
+	$data_COMMANDE[] = $vals[COMMANDE] ;
+
+$data_MONTANT_COMMANDE = array();
+foreach($cmd_rubis as $vals)
+	$data_MONTANT_COMMANDE[] = $vals[MONTANT_COMMANDE] ;
+
+$data_DEVIS = array();
+foreach($cmd_rubis as $vals)
+	$data_DEVIS[] = $vals[DEVIS] ;
+
+$data_TAUX = array();
+foreach($cmd_rubis as $vals)
+	$data_TAUX[] = sprintf('%0.1f',$vals[COMMANDE]*100 / $vals[DEVIS]) ;
+
+
+// Setup the graph
+$graph = new Graph(1200,650);
+$graph->SetMarginColor('white');
+$graph->SetFrame(false);
+$graph->SetMargin(70,50,100,60);
+$graph->title->Set('Evolution mois par mois');
+
+$graph->SetScale('textlin');
+$graph->SetY2Scale('lin');
+$graph->yaxis->SetColor('blue');
+$graph->y2axis->SetColor('red');
+$graph->yaxis->title->Set("Montant des commandes");
+$graph->yaxis->SetTitlemargin(50);
+$graph->yaxis->title->SetColor('navy');
+$graph->y2axis->title->Set("Devis/Commande");
+$graph->y2axis->title->SetColor('red');
+
+$graph->yaxis->HideZeroLabel();
+$graph->ygrid->SetFill(true,'#EFEFEF@0.5','#BBCCFF@0.5');
+$graph->xgrid->Show();
+$graph->xaxis->SetLabelAngle(90);
+$graph->xaxis->SetTickLabels(array_keys($cmd_rubis));
+
+$graph->legend->SetShadow('gray@0.4',5);
+$graph->legend->SetPos(0.21,0,'right','top');
+
+
+
+// Create the bar plots
+$bar_cmd	= new BarPlot($data_COMMANDE);
+$bar_devis	= new BarPlot($data_DEVIS);
+$line_ca	= new LinePlot($data_MONTANT_COMMANDE);
+$line_taux	= new LinePlot($data_TAUX);
+
+
+// Create the grouped bar plot
+$acc_cmd_devis		= new AccBarPlot(array($bar_devis,$bar_cmd));
+$acc_cmd_devis->SetWidth(0.75);
+
+$graph->Add($line_ca);
+$graph->AddY2($acc_cmd_devis);
+$graph->AddY2($line_taux);
+
+
+// Create the commande bar
+$bar_cmd->SetFillColor("red");
+$bar_cmd->SetLegend('Commandes réalisées');
+$bar_cmd->value->SetColor('#55000'); 
+$bar_cmd->value->SetFormat('%d');
+$bar_cmd->value->SetFont( FF_FONT1, FS_BOLD); 
+$bar_cmd->value->Show();
+
+
+// Create the devis bar;
+$bar_devis->SetFillColor("orange");
+$bar_devis->SetLegend('Devis réalisés');
+$bar_devis->value->SetColor('red'); 
+$bar_devis->value->SetFormat('%d');
+$bar_devis->value->SetFont( FF_FONT1, FS_BOLD); 
+$bar_devis->value->Show();
+
+// Create the CA line
+$line_ca->SetColor('blue');
+$line_ca->SetLegend('Montant des commandes');
+$line_ca->value->SetColor('navy'); 
+$line_ca->value->SetFormat('%d');
+$line_ca->value->SetFont( FF_FONT1, FS_BOLD); 
+$line_ca->value->Show();
+$line_ca->SetBarCenter();
+$line_ca->mark->SetTYPE( MARK_SQUARE);
+$line_ca->mark->SetColor("navy");
+
+// create TAUX line 
+$line_taux->SetColor('purple');
+$line_taux->SetLegend('Taux de tranformation devis/cmd');
+$line_taux->value->SetColor('purple'); 
+$line_taux->value->SetFormat('%d%%');
+$line_taux->value->SetFont( FF_FONT1, FS_BOLD); 
+$line_taux->value->Show();
+$line_taux->SetBarCenter();
+$line_taux->mark->SetTYPE( MARK_DIAMOND);
+$line_taux->mark->SetColor('purple');
+$line_taux->mark->SetFillColor('purple');
+
+// Output line
+$graph->Stroke();
+?>
---
+++ trunk/devis2/stats_devis.php
@@ -0,0 +1,217 @@
+<?php
+
+include('../inc/config.php');
+include('../inc/iCalParser/ical-parser-class.php');
+set_include_path(get_include_path().PATH_SEPARATOR.'../inc'); // ajoute le chemin d'acces a Spreadsheet/Excel
+require_once '../inc/Spreadsheet/Excel/Writer.php';
+
+$mysql    = mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS) or die("Impossible de se connecter");
+$database = mysql_select_db(MYSQL_BASE) or die("Impossible de se choisir la base");
+
+$i=0;
+define('COL_DATE',$i++);
+define('COL_RDV',$i++);
+define('COL_VISITE',$i++);
+define('COL_PROSPECT',$i++);
+define('COL_DEVIS_REALISE',$i++);
+define('COL_CMD_REALISE',$i++);
+define('COL_RATIO',$i++);
+define('COL_MT_CMD',$i++);
+
+$cumul = array('RDV' => array() , 'VISITE' => array(),'PROSPECT' => array() );
+$ftp = ftp_connect(FTP_RDV_HOST);
+
+// Identification avec un nom d'utilisateur et un mot de passe
+$login_result = ftp_login($ftp, FTP_RDV_USER, FTP_RDV_PASS);
+// Vérification de la connexion
+if ((!$ftp) || (!$login_result)) die("La connexion FTP a échoué !");
+
+
+foreach (array('expo_archive.ics','expo.ics') as $fichier) {
+	if (ftp_get($ftp, $fichier, $fichier, FTP_BINARY)) {
+	   // le fichier est bien la, on le traite
+		
+		$ical = new iCal();
+		$events = $ical->iCalDecoder($fichier);
+		
+		foreach ($events as $e) {
+			if (array_key_exists('SUMMARY',$e) && eregi('^(RDV|VISITE|PROSPECT)',$e['SUMMARY'],$regs)) { //SUMMARY,DTSTART
+				//on traite le rdv ou visite
+				$type = strtoupper($regs[1]);
+
+				$nom_cle_start = '';
+				foreach($e as $key=>$val) {
+						if (substr($key,0,8) == 'DTSTART;') {
+							$nom_cle_start = $key;
+							break;
+						}
+				}
+
+				if (isset($e[$nom_cle_start])) {
+					$date = substr($e[$nom_cle_start],0,6) ;
+					
+					if (isset($cumul[$type][$date]))
+						$cumul[$type][$date] += 1;
+					else
+						$cumul[$type][$date] = 1;
+				}
+			}
+		}
+	} else {
+		die("Impossible de récupérer le fichier des calendrier");
+	}
+} // for each fichier
+
+// Fermeture du flux FTP
+ftp_close($ftp);
+
+
+// Creating a workbook
+$workbook = new Spreadsheet_Excel_Writer();
+
+// sending HTTP headers
+$workbook->send('stat-expo.xls');
+
+// Creating a worksheet
+$worksheet =& $workbook->addWorksheet('Statistique de la salle expo');
+$workbook->setCustomColor(12, 220, 220, 220);
+
+$format_title =& $workbook->addFormat(array('bold'=>1 , 'fgcolor'=>12 , 'bordercolor'=>'black' ));
+$format_cell  =& $workbook->addFormat(array('bordercolor'=>'black'));
+$format_pourcentage  =& $workbook->addFormat(array('bold'=>1 , 'fgcolor'=>12 , 'bordercolor'=>'black' ));
+$format_pourcentage->setNumFormat('0.0%');
+
+// La premiere ligne
+$worksheet->write(0,COL_RDV,			'RDV',$format_title);
+$worksheet->write(0,COL_VISITE, 		'VISITE',$format_title);
+$worksheet->write(0,COL_PROSPECT, 		'PROSPECT',$format_title);
+$worksheet->write(0,COL_DEVIS_REALISE,	'Devis réalisé',$format_title);
+$worksheet->write(0,COL_CMD_REALISE,	'Cmd passée',$format_title);
+$worksheet->write(0,COL_RATIO,			'Ratio devis/cmd %',$format_title);
+$worksheet->write(0,COL_MT_CMD,			'Montant des cmd',$format_title);
+$worksheet->setColumn(0,COL_MT_CMD,17);
+
+
+// calcul du taux de devis/cmd de la salle
+$sql = <<<EOT
+SELECT  DISTINCT ( DATE_FORMAT( `date` , '%b %Y' )) AS date_formater,
+		DATE_FORMAT( `date` , '%Y%m' ) AS date_formater_ical,
+		DATE_FORMAT( `date` , '%Y' ) AS annee,
+        COUNT(id) AS nb_devis
+FROM devis
+GROUP BY date_formater
+ORDER BY `date` ASC
+EOT;
+
+$res = mysql_query($sql) or die("Ne peux pas trouver le nombre de devis ".mysql_error());
+
+$sql2 = <<<EOT
+SELECT  DISTINCT ( DATE_FORMAT( `date` , '%b %Y' )) AS date_formater,
+        COUNT(num_cmd_rubis) AS nb_cmd_rubis,
+		SUM(mtht_cmd_rubis) AS montant_cmd
+FROM devis
+WHERE     num_cmd_rubis NOT LIKE 'ANNULE'
+      AND num_cmd_rubis NOT LIKE 'SUSPENDU'
+      AND num_cmd_rubis IS NOT NULL
+      AND num_cmd_rubis <> ''
+GROUP BY date_formater
+ORDER BY `date` ASC
+EOT;
+
+$res2 = mysql_query($sql2) or die("Ne peux pas trouver le nombre de cmd ".mysql_error());
+$cmd_rubis = array();
+while($row = mysql_fetch_array($res2)) {
+	$cmd_rubis[$row['date_formater']] = array($row['nb_cmd_rubis'],$row['montant_cmd']);
+}
+
+$i = 1;
+$old_year = 0;
+$last_ligne = 2 ;
+$ligne_annee = array();
+while($row = mysql_fetch_array($res)) {
+	if ($old_year != $row['annee'] && $i!=1) { # on change d'année, faire un calcul intermediaire (sauf pour le premier passage)
+		array_push($ligne_annee,$i);
+		write_total_ligne($old_year);
+	}
+
+	$worksheet->write( $i, COL_DATE ,			$row['date_formater'],$format_title);
+	$worksheet->write( $i, COL_DEVIS_REALISE ,  $row['nb_devis'],$format_cell);
+	$worksheet->write( $i, COL_CMD_REALISE , 	isset($cmd_rubis[$row['date_formater']][0]) ? $cmd_rubis[$row['date_formater']][0] : 0 ,$format_cell);
+	$worksheet->write( $i, COL_MT_CMD , 		isset($cmd_rubis[$row['date_formater']][1]) ? $cmd_rubis[$row['date_formater']][1] : 0 ,$format_cell);
+	$ligne = $i+1 ;
+	$worksheet->writeFormula($i, COL_RATIO, '='.excel_column(COL_CMD_REALISE).$ligne.'/'.excel_column(COL_DEVIS_REALISE).$ligne ,$format_pourcentage);
+	
+
+	if (isset($cumul['RDV'][$row['date_formater_ical']])) {
+		$worksheet->write( $i, COL_RDV , $cumul['RDV'][$row['date_formater_ical']],$format_cell);
+	}
+	if (isset($cumul['VISITE'][$row['date_formater_ical']])) {
+		$worksheet->write( $i, COL_VISITE, $cumul['VISITE'][$row['date_formater_ical']],$format_cell);
+	}
+	if (isset($cumul['PROSPECT'][$row['date_formater_ical']])) {
+		$worksheet->write( $i, COL_PROSPECT, $cumul['PROSPECT'][$row['date_formater_ical']],$format_cell);
+	}
+
+	$old_year = $row['annee'];
+	$i++;
+}
+
+array_push($ligne_annee,$i);
+write_total_ligne($old_year);
+
+
+
+
+// ecrit le total final du cumul des années
+$worksheet->write(			$i, COL_DATE,	"Total",$format_title);
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_RDV).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_RDV,	"=SUM($formula)",$format_title) ;
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_VISITE).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_VISITE,	"=SUM($formula)",$format_title) ;
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_PROSPECT).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_PROSPECT,"=SUM($formula)",$format_title) ;
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_DEVIS_REALISE).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_DEVIS_REALISE,	"=SUM($formula)",$format_title) ;
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_CMD_REALISE).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_CMD_REALISE,	"=SUM($formula)",$format_title) ;
+$formula = '' ; foreach ($ligne_annee as $ligne) $formula .= excel_column(COL_MT_CMD).($ligne+1).';' ; $formula = ereg_replace(';+$','',$formula);
+$worksheet->writeFormula(	$i, COL_MT_CMD,	"=SUM($formula)",$format_title) ;
+//$col = excel_column($i) ;
+$worksheet->writeFormula($i, COL_RATIO,		'='.excel_column(COL_CMD_REALISE).($i+1).'/'.excel_column(COL_DEVIS_REALISE).($i+1) ,$format_pourcentage);
+
+
+
+
+
+// Let's send the file
+$workbook->close();
+
+
+function write_total_ligne($annee) {
+	global $i,$worksheet,$last_ligne,$format_title,$format_pourcentage ;
+	$ligne = $i ;
+	$worksheet->write(		 $i,COL_DATE,			"Total $annee",$format_title);
+	$worksheet->writeFormula($i,COL_RDV,			'=SUM('.excel_column(COL_RDV).$last_ligne.				':'.excel_column(COL_RDV).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_VISITE,			'=SUM('.excel_column(COL_VISITE).$last_ligne.			':'.excel_column(COL_VISITE).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_PROSPECT,		'=SUM('.excel_column(COL_PROSPECT).$last_ligne.			':'.excel_column(COL_PROSPECT).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_DEVIS_REALISE,	'=SUM('.excel_column(COL_DEVIS_REALISE).$last_ligne.	':'.excel_column(COL_DEVIS_REALISE).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_CMD_REALISE,	'=SUM('.excel_column(COL_CMD_REALISE).$last_ligne.		':'.excel_column(COL_CMD_REALISE).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_MT_CMD,			'=SUM('.excel_column(COL_MT_CMD).$last_ligne.			':'.excel_column(COL_MT_CMD).$i.')' ,$format_title);
+	$worksheet->writeFormula($i,COL_RATIO,			'='.excel_column(COL_CMD_REALISE).($i+1).'/'.excel_column(COL_DEVIS_REALISE).($i+1) ,$format_pourcentage);
+
+	$i++;
+	$last_ligne = $i+1 ;
+}
+
+
+function excel_column($col_number) {
+	if( ($col_number < 0) || ($col_number > 701)) die('Column must be between 0(A) and 701(ZZ)');
+	if($col_number < 26) {
+		return(chr(ord('A') + $col_number));
+	} else {
+		$remainder = floor($col_number / 26) - 1;
+		return(chr(ord('A') + $remainder) . excel_column($col_number % 26));
+	}
+}
+
+?>
