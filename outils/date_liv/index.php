e265b319494e9e4d5847c96bb0e058b671245432
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2009-07-24 07:48:26 +0000

    date de mise à dispo : on prévient ausis les vendeurs

--- outils/date_liv/index.php
+++ outils/date_liv/index.php
@@ -15,7 +15,7 @@ if (	isset($_POST['what'])
 	$loginor  = odbc_connect(LOGINOR_DSN,LOGINOR_USER,LOGINOR_PASS) or die("Impossible de se connecter à Loginor via ODBC ($LOGINOR_DSN)");
 
 	$sql = <<<EOT
-select DETAIL_BON.NOFOU,NOMFO,DETAIL_BON.CFCLI,NOMCL,CFDLS,CFDLA,CFDLM,CFDLJ,REFFO,CFDE1,CFDE2,CFDE3,CFQTE,PRINE,MONHT,CFART,CFCLI,CFCLB,RFCSB,CFLIG
+select DETAIL_BON.NOFOU,NOMFO,DETAIL_BON.CFCLI,NOMCL,CFDLS,CFDLA,CFDLM,CFDLJ,REFFO,CFDE1,CFDE2,CFDE3,CFQTE,PRINE,MONHT,CFART,CFCLI,CFCLB,RFCSB,CFLIG,CDE_CLIENT_ENTETE.LIVSB,CDE_CLIENT_ENTETE.NOMSB
 from	${LOGINOR_PREFIX_BASE}GESTCOM.ACFDETP1 DETAIL_BON
 		left join  ${LOGINOR_PREFIX_BASE}GESTCOM.AFOURNP1 FOURNISSEUR
 			on		DETAIL_BON.NOFOU	= FOURNISSEUR.NOFOU
@@ -44,6 +44,7 @@ EOT;
 	$four			= array();
 	$four_nom		= array();
 	$adhs			= array();
+	$vendeurs		= array();
 	$nb_result		= 0;
 
 	while($row = odbc_fetch_array($res)) {
@@ -62,17 +63,28 @@ EOT;
 			// on cree un nouveau tableu
 			$adhs[$row['CFCLI']] = array($row);
 		}
+
+		if (array_key_exists($row['LIVSB'],$vendeurs)) { //on a deja rencontré le vendeur
+			// on push dans le tableau existant
+			array_push($vendeurs[$row['LIVSB']],$row);
+		} else {
+			// on cree un nouveau tableu
+			$vendeurs[$row['LIVSB']] = array($row);
+		}
 	}
 
 	if ($nb_result <= 0)
 		$message .= "<div class=\"message\" style=\"color:red;\">Aucune lignes concern&eacute;e pour le bon $num_cde</div>\n";
 
 	//print_r($adhs);exit;
+	//print_r($vendeurs);exit;
 
 	//si la commande ne correspond qu'a un seul fourn --> on envoi les mail aux adh
 	if (sizeof($four) == 1) {
 		$mysql		= mysql_connect(MYSQL_HOST, MYSQL_USER, MYSQL_PASS) or die("Impossible de se connecter à MySQL");
 		$database	= mysql_select_db(MYSQL_BASE) or die("Impossible de se choisir la base MySQL");
+
+
 
 		foreach ($adhs as $no_cli => $lignes) { // pour chaque adh on envoi un mail concernant ses produits
 			$sql		= "SELECT email,nom FROM artisan WHERE numero='$no_cli' LIMIT 0,1";
@@ -165,11 +177,108 @@ EOT;
 					else 
 						$message .= "<div class=\"message\" style=\"color:red;\">Erreur dans l'envoi de l'email à $row[nom] ($row[email])</div>\n";
 						
+						
 				}
 			} else { // fin if il a un email
 				$message .= "<div class=\"message\" style=\"color:red;\">Erreur $row[nom] n'a pas d'email</div>\n";
 			}
 		}// fin pour chaque adhérents
+
+
+
+
+
+		if ($_POST['what'] == 'mise_a_dispo') { // pour les mises à dispo, on prévient aussi les vendeurs
+		foreach ($vendeurs as $no_vendeur => $lignes) { // pour chaque vendeur on envoi un mail concernant ses produits
+			$sql		= "SELECT email,prenom,nom FROM employe WHERE code_vendeur='$no_vendeur' LIMIT 0,1";
+
+			$res = mysql_query($sql) or die ("Ne peux pas récupérer l'email du vendeur : ".mysql_error());
+			$row = mysql_fetch_array($res);
+			//$row['email']='ryo@wanadoo.fr';	// pour le debug
+			//$row['nom']='benjamin';			// pour le debug
+			if ($row['email']) { // un email de renseigné --> on peut envoyer
+				$nom_four = trim($lignes[0]['NOMFO']);
+
+				$html = <<<EOT
+<b>Les articles suivant du fournisseur <u>$nom_four</u> viennent d'être mis à disposition à la coopérative</b>
+<br/><br/>
+<table border="1" cellpadding="3" cellspacing="0">
+<tr>
+		<th>Adh&eacute;rent</th>
+        <th>N° Cde</th>
+        <th>Référence Cde</th>
+        <th>Code</th>
+        <th>Fourn</th>
+        <th>Ref fourn.</th>
+        <th>Designation</th>
+        <th>Date liv.</th>
+        <th>Qte</th>
+        <th>P.U.</th>
+        <th>Tot.</th>
+</tr>
+EOT;
+
+				$cde_adh = array();
+				$cde_ligne_traitee = array();
+				$need_email = FALSE;
+				foreach ($lignes as $idx => $lig) { // pour chaque ligne des bons adhérents
+
+					// on vérifie si un email n'a pas déjà été envoyé
+					if (mysql_num_rows(mysql_query("SELECT id FROM mise_a_dispo_sent WHERE no_cde_fourn='$num_cde' AND code_fourn='".mysql_escape_string($four[0])."' AND ligne='".mysql_escape_string($lig['CFLIG'])."' LIMIT 0,1"))) { // si oui --> on passe à la ligne suivante
+						continue;
+					}
+
+					$need_email = TRUE;
+					array_push($cde_ligne_traitee,$lig['CFLIG']); // on enregsitre la ligne traitee pour la base de donnée
+					array_push($cde_adh,$lig['CFCLB']);
+					$designation	= $lig['CFDE1'];
+					$designation	.= $lig['CFDE2'] ? '<br>'.$lig['CFDE2']:'';
+					$designation	.= $lig['CFDE3'] ? '<br>'.$lig['CFDE3']:'';
+					$lig['CFQTE']	= sprintf('%0.2f',$lig['CFQTE']);
+					$lig['PRINE']	= sprintf('%0.2f',$lig['PRINE']);
+					$lig['REFFO']	= $lig['REFFO'] ? $lig['REFFO'] : '&nbsp;';
+					$html .= <<<EOT
+<tr>
+	<td>$lig[NOMSB]</td>
+	<td>$lig[CFCLB]</td>
+	<td>$lig[RFCSB]</td>
+	<td>$lig[CFART]</td>
+	<td>$lig[NOMFO]</td>
+	<td>$lig[REFFO]</td>
+	<td>$designation</td>
+	<td style="font-weight:bold;">$lig[CFDLJ]/$lig[CFDLM]/$lig[CFDLS]$lig[CFDLA]</td>
+	<td>$lig[CFQTE]</td>
+	<td>$lig[PRINE]</td>
+	<td>$lig[MONHT]</td>
+</tr>
+EOT;
+				} // fin pour chaque ligne
+				$html .= "</table>";
+
+				if ($need_email) {
+					$mail = new SMTP;
+					$mail->Delivery('relay');
+					$mail->Relay(SMTP_SERVEUR);
+					//$mail->AddTo('ryo@wanadoo.fr', 'Ben') or die("Erreur d'ajour de destinataire"); // pour les tests
+					$mail->AddTo($row['email'], $row['nom']) or die("Erreur d'ajout de destinataire");
+					$mail->From('rachel.kerzulec@coopmcs.com','Rachel Kerzulec');
+
+					$mail->Html($html);
+					//echo $row['nom']."\n<br>".$html."<br><br><br>";
+
+					if ($mail->Send("MCS : Dates de livraison du fournisseur $nom_four : Cde : ".join(', ',$cde_adh)))
+						$message .= "<div class=\"message\" style=\"color:green;\">Email correctement envoyé au vendeur $row[prenom] ($row[email])</div>\n";
+					else 
+						$message .= "<div class=\"message\" style=\"color:red;\">Erreur dans l'envoi de l'email au vendeur $row[prenom] ($row[email])</div>\n";
+					
+				}
+			} else { // fin if il a un email
+				$message .= "<div class=\"message\" style=\"color:red;\">Erreur le vendeur $row[prenom] $row[nom] n'a pas d'email</div>\n";
+			}
+		}// fin pour chaque adhérents
+		} // fin mise a dispo pour les vendeurs
+
+
 	}
 	elseif (sizeof($four) > 1) { // cas ou plusieurs fournisseur ont le meme n° de cde
 		$select_fou = "<select name=\"no_fou\">";
