fb1fb57576ffb0d9455ebdc2a8e5747f9b167fe6
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2011-05-19 13:20:12 +0000

    - expo : ajout de l'application pour éditer des étiquettes

---
+++ devis2/ajax_etiquette_expo.php
@@ -0,0 +1,97 @@
+<?
+include('../inc/config.php');
+
+
+if ($_GET['what'] == 'get_detail_box' && isset($_GET['val']) && $_GET['val']) { ////// RECHERCHE DES INFO VIA LA REF FOURNISSEUR DANS SQLITE
+	
+	// va récupérer la liste articles (et des infos) présent dans le box
+	$sql = <<<EOT
+select
+	A.NOART,A.DESI1,
+	AF.NOFOU,AF.REFFO,
+	F.NOMFO,
+	S.LOCAL, S.LOCA2, S.LOCA3
+from			AFAGESTCOM.AARTICP1 A
+	left join	AFAGESTCOM.AARFOUP1 AF
+		on  A.NOART=AF.NOART and A.FOUR1=AF.NOFOU
+	left join	AFAGESTCOM.ASTOFIP1 S
+		on  A.NOART=S.NOART and S.DEPOT='AFA'
+	left join	AFAGESTCOM.AFOURNP1 F
+		on  A.FOUR1=F.NOFOU
+where
+		LOCAL like '%$_GET[val]%'
+	or	LOCA2 like '%$_GET[val]%'
+	or	LOCA3 like '%$_GET[val]%'
+EOT;
+
+	$loginor  = odbc_connect(LOGINOR_DSN,LOGINOR_USER,LOGINOR_PASS) or die("Impossible de se connecter à Loginor via ODBC ($LOGINOR_DSN)");
+	$res = odbc_exec($loginor,$sql)  or die("Impossible de lancer la requete : $sql");
+
+	// va récupérer la liste des prix des articles du box
+	$articles = array();
+	while($row = odbc_fetch_array($res)) {
+		$row = array_map('trim',$row);
+		$qte = 0;
+		foreach (split('/',join('/',array($row['LOCAL'],$row['LOCA2'],$row['LOCA3']))) as $local) { // pour chaque localisation
+			if ($local) {
+				if (preg_match("/$_GET[val](-(\\d+))?/",$local,$matches)) {
+					$qte += isset($matches[2]) ? $matches[2] : 1; // on test la quantité "X25-3"
+				}
+			}
+		}
+
+		$articles["$row[NOFOU].$row[REFFO]"] = array(	'designation'		=> $row['DESI1'],
+														'code_fournisseur'	=> $row['NOFOU'],
+														'fournisseur'		=> $row['NOMFO'],
+														'reference'			=> $row['REFFO'],
+														'qte'				=> $qte,
+														'code_expo'			=> $row['NOART']
+													);
+	}
+
+	//print_r($articles);
+
+	$tmp = array();
+	foreach ($articles as $article => $data) {
+		array_push($tmp,"(A.ACTIV<>'00S' and AF.NOFOU='$data[code_fournisseur]' and AF.REFFO='$data[reference]')");
+	}
+	$tmp = join(' OR ',$tmp);
+	$sql = <<<EOT
+select
+	A.NOART,
+	AF.NOFOU,AF.REFFO,
+	PV.PVEN6 as PX_PUBLIC, (PV.PVEN1 * 1.5) as PX_PUBLIC_CALCULE
+from	AFAGESTCOM.AARFOUP1 AF
+		left join AFAGESTCOM.ATARPVP1 PV
+			on  PV.PVT09='E' and AF.NOART=PV.NOART
+		left join AFAGESTCOM.AARTICP1 A
+			on  PV.NOART=A.NOART
+where
+	$tmp
+EOT;
+
+	//echo "\n$sql";
+
+	$res = odbc_exec($loginor,$sql)  or die("Impossible de lancer la requete : $sql");
+	while($row = odbc_fetch_array($res)) {
+		$row = array_map('trim',$row);
+		$articles["$row[NOFOU].$row[REFFO]"]['code_article'] = $row['NOART'];
+		if	($row['PX_PUBLIC'] > 0)
+			$articles["$row[NOFOU].$row[REFFO]"]['px_public'] = round(min($row['PX_PUBLIC'],$row['PX_PUBLIC_CALCULE']),2); // si un prix public est renseigné, on prend le moins cher des deux
+		else
+			$articles["$row[NOFOU].$row[REFFO]"]['px_public'] = round($row['PX_PUBLIC_CALCULE'],2);	// sinon on prend le prix calculé avec la formule
+	}
+
+	echo json_encode($articles);
+} // fin 'get_detail_box'
+
+
+
+
+// CAS PAR DEFAUT
+else {
+	echo "Aucune procedure selectionnée";
+}
+
+
+?>
---
+++ devis2/etiquette_expo.php
@@ -0,0 +1,205 @@
+<?
+include('../inc/config.php');
+
+$start = microtime(true);
+
+?><html>
+<head>
+<title>Création d'étiquette expo</title>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script language="javascript">
+
+function refresh_etiquette(sel,id) {
+	var box = sel[sel.selectedIndex].value;
+
+	if (box) {
+		$('#loading').css('visibility','visible'); // affiche le loading
+		$('#etiquette'+id).html('');
+		//	alert(box);	
+		$.getJSON('ajax_etiquette_expo.php', { 'what':'get_detail_box', 'val': box  } ,
+			function(data){
+				//data.reference
+				var html = '<table class="articles"><caption>Box '+box+'</caption>';
+				var total = 0;
+				for(article in data) {
+					var erreur = '';
+					if (!data[article].code_article)
+						erreur += "Aucun code MCS n'a été trouvé pour cette référence fournisseur (Code expo : "+data[article].code_expo+")";
+
+					html += '<tr><td class="fournisseur">'+
+						//	data[article].code_article+'<br/>'+
+						//	data[article].code_expo+'<br/>'+
+							'<div class="fournisseur">'+data[article].fournisseur+'</div>'+
+							'<div class="reference">'+data[article].reference+'</div></td><td class="designation">'+
+							(data[article].qte > 1 ? '<strong>x'+data[article].qte+'</strong> ':'') + data[article].designation+'<div class="erreur">'+erreur+'</div></td><td class="prix" nowrap="nowrap">'+
+							(data[article].qte > 1 ? '<span style="font-style:normal;">'+data[article].qte+'x</span> ':'') + (data[article].px_public ? data[article].px_public.toFixed(2) + '&nbsp;&euro;':'NC')+'</td></tr>';
+
+					total += data[article].qte * (data[article].px_public ? data[article].px_public.toFixed(2):0);
+				}
+				html += '<tr><td class="prix-conseille">Prix conseillés</td><td class="total" colspan="2">Montant total du box : '+total.toFixed(2)+'&nbsp;&euro;</td></tr></table>';
+				$('#etiquette'+id).append(html);
+
+				$('#loading').css('visibility','hidden'); // supprime le loading
+			}
+		);
+	}
+}
+
+</script>
+
+<style>
+
+body {
+    font-family: verdana;
+    font-size: 0.8em;
+}
+div.choix-box {
+    border: medium none;
+    float: left;
+    text-align: center;
+    width: 45%;
+}
+div#loading {
+    text-align: center;
+    visibility: hidden;
+    width: 100%;
+}
+div.debug {
+    color: grey;
+    display: none;
+    font-size: 0.7em;
+}
+table#etiquette {
+    font-family: verdana;
+    font-size: 8px;
+    height: 190mm;
+    width: 285mm;
+}
+#etiquette1, #etiquette2 {
+    height: 100%;
+    padding: 0 0.5cm 0.5cm;
+    vertical-align: top;
+    width: 50%;
+}
+td#etiquette1 {
+    border-right: 1px dotted black;
+}
+td#etiquette2 {
+}
+table.articles {
+    border-collapse: collapse;
+    width: 100%;
+}
+table.articles caption {
+    font-size: 1.5em;
+    font-weight: bold;
+    margin: 0 0 5px;
+    text-align: center;
+}
+div.fournisseur {
+    font-weight: bold;
+}
+td.prix {
+    font-weight: bold;
+    text-align: right;
+}
+td.total {
+    text-align: right;
+}
+table.articles td {
+    border-bottom: 1px dotted black;
+    font-size: 0.7em;
+    vertical-align: top;
+}
+.erreur {
+    color: red;
+}
+td.designation {
+    font-style: italic;
+    padding-left: 0.5cm;
+}
+.prix-conseille {
+    font-size: 1.5em;
+    text-align: left;
+}
+
+@media print {
+	.hide_when_print { display:none; }
+}
+
+</style>
+
+<body>
+<!-- menu de naviguation -->
+<? include('../inc/naviguation.php'); ?>
+
+<form method="post" action="" name="etiquette_expo" id="etiquette_expo" class="hide_when_print">
+<fieldset id="choix_box">
+    <legend>Listes des box :</legend>
+<?
+	// va récupérer la liste des box dispo dans les cases localisation des fiches de stocks
+	$sql = <<<EOT
+select
+	S.LOCAL,S.LOCA2,S.LOCA3
+from ${LOGINOR_PREFIX_BASE}GESTCOM.AARTICP1 A
+	left join ${LOGINOR_PREFIX_BASE}GESTCOM.ASTOFIP1 S
+		on  A.NOART=S.NOART and S.DEPOT='${LOGINOR_DEPOT}'
+where
+	A.ACTIV = '00S'
+and (LOCAL<>'' or LOCA2<>'' or LOCA3<>'')
+group by LOCAL,LOCA2,LOCA3
+EOT;
+
+	$locals = array();
+
+	echo "<div class='debug'>Avant connexion : ".(microtime(true) - $start)."</div>";
+	$loginor  = odbc_connect(LOGINOR_DSN,LOGINOR_USER,LOGINOR_PASS) or die("Impossible de se connecter à Loginor via ODBC ($LOGINOR_DSN)");
+	echo "<div class='debug'>Apres connexion : ".(microtime(true) - $start)."</div>";
+	$res = odbc_exec($loginor,$sql)  or die("Impossible de lancer la requete : $sql");
+	echo "<div class='debug'>Apres requette : ".(microtime(true) - $start)."</div>";
+
+	while($row = odbc_fetch_array($res)) {
+		foreach (array('LOCAL','LOCA2','LOCA3') as $field) {
+			foreach (split('/',$row[$field]) as $local) {
+				$box = substr(trim(strtoupper($local)),0,3);
+				if (substr($box,0,1) == 'X')
+					$locals[$box] = 1;
+			}
+		}
+	}
+
+	echo "<div class='debug'>Apres fetch : ".(microtime(true) - $start)."</div>";
+	ksort($locals);
+	echo "<div class='debug'>Apres ksort : ".(microtime(true) - $start)."</div>";
+	
+	for($i=1 ; $i<=2 ; $i++) { ?>
+		<div id="choix-box-<?=$i?>" class="choix-box">Box <?=$i?> :
+			<select onchange="refresh_etiquette(this,<?=$i?>);">
+				<option value="">Choix d'un emplacement</option>
+<?				foreach($locals as $box => $val) {
+	//			for($j=1 ; $j<=99 ; $j++) {
+?>
+					<option value="<?=$box?>"><?=$box?></option>
+<?				} ?>
+			</select>
+		</div>
+<?	}
+	echo "<div class='debug'>Apres affichage : ".(microtime(true) - $start)."</div>";	
+?>
+</fieldset>
+</form>
+
+<div id="loading" class="hide_when_print"><img src="gfx/loading4.gif"/> Patientez un instant ...</div>
+
+<table id="etiquette">
+<tr>
+	<td id="etiquette1"></td>
+	<td id="etiquette2"></td>
+</tr>
+</table>
+
+</body>
+</html>
+<?
+?>
