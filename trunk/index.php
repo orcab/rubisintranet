22b9162f794b54955c86889c91dbe281ad91bb15
Benjamin.Poulain <Benjamin.Poulain@f0075f7e-b220-11dd-9501-15ef518ea1c2>
2011-09-15 10:17:54 +0000

    - ajout des evemneents de la semaines sur la page d'accueil de l'intranet

--- trunk/index.php
+++ trunk/index.php
@@ -32,6 +32,27 @@ img:hover {
 	-moz-transform: scale(1.1);
 }
 
+div#header {
+	width:96%;
+	background-image:-moz-linear-gradient( bottom , #fdfdfd, #ddd );
+	margin-bottom:10px;
+	height:30px;
+	padding-left:50px;
+	font-weight:bold;
+	padding-top:10px;
+}
+
+div#footer {
+	background-image:-moz-linear-gradient( top , #fdfdfd, #eee );
+	width:96%;
+	margin-top:10px;
+	height:150px;
+	padding-left:50px;
+	font-weight:bold;
+	padding-top:5px;
+	font-size:0.8em;
+}
+
 
 </style>
 
@@ -41,7 +62,7 @@ img:hover {
 
 </head>
 <body style="margin:0px;padding:0px;">
-<div style="width:100%;background-color:#DDD;margin-bottom:10px;height:30px;padding-left:50px;font-weight:bold;padding-top:10px;">Intranet <?=SOCIETE?></div>
+<div id="header">Intranet <?=SOCIETE?></div>
 
 <center>
 <table style="width:70%;text-align:center;border:solid 1px #AAA;">
@@ -64,11 +85,101 @@ img:hover {
 </table>
 </center>
 
+<div id="footer">
+	<h2 style="font-size:0.9em;color:grey;font-weight:normal;">Evenements à MCS dans la semaine</h2>
+	<?	// charge le fichier json des evenements
+		$ini_filename = 'scripts/ical2json.ini';
+		if (file_exists($ini_filename)) {
+			$ini = parse_ini_file($ini_filename,true);
+			if (file_exists($ini['files']['json_output'])) {
+				$json = join('',file($ini['files']['json_output']));
+				$events_not_ordered = json_decode($json,true); // assoc
+				
 
+				// on classe les evenements dans l'ordre de debut
+				$events_ordered = array();
+				foreach ($events_not_ordered as $uid=>$event) {
+					if (isset($event['DTSTART;TZID']) &&
+						preg_match('/^Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTSTART;TZID'],$matches))
+						$date_start = $matches;
+					if (isset($event['DTSTART;VALUE']) &&
+						preg_match('/^DATE;TZID=Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTSTART;VALUE'],$matches))
+						$date_start = $matches;
 
-<div style="width:100%;background-color:#DDD;margin-top:10px;height:30px;padding-left:50px;font-weight:bold;padding-top:10px;">
-	<a href="wiki/">[Wiki]</b></a>&nbsp;&nbsp;&nbsp;&nbsp;
+					if (!isset($matches[4])) { // si l'heure n'est pas présente, on l'ajoute
+						$matches[4]='00';
+						$matches[5]='00';
+						$matches[6]='00';
+					}
+
+					$events_ordered[$uid] = $matches[1].$matches[2].$matches[3].$matches[4].$matches[5].$matches[6] ;
+				}
+
+				asort($events_ordered);
+				$events = array();
+				foreach ($events_ordered as $uid=>$date) {
+					$events[] = $events_not_ordered[$uid];
+				}
+
+				// on affiche les evenements classés
+				foreach ($events as $uid=>$event) {
+					if (isset($event['DTSTART;TZID']) &&
+						preg_match('/^Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTSTART;TZID'],$matches))
+						$date_start = $matches;
+					if (isset($event['DTSTART;VALUE']) &&
+						preg_match('/^DATE;TZID=Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTSTART;VALUE'],$matches))
+						$date_start = $matches;
+
+					if (isset($event['DTEND;TZID']) &&
+						preg_match('/^Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTEND;TZID'],$matches))
+						$date_end = $matches;
+					if (isset($event['DTEND;VALUE']) &&
+						preg_match('/^DATE;TZID=Europe\/Paris:(\d{4})(\d{2})(\d{2})(?:T(\d{2})(\d{2})(\d{2}))?$/',$event['DTEND;VALUE'],$matches))
+						$date_end = $matches;
+
+					$date_start_time = mktime(	isset($date_start[4])?$date_start[4]:0,	// hour
+												isset($date_start[5])?$date_start[5]:0,	// min
+												isset($date_start[6])?$date_start[6]:0,	// sec
+												$date_start[2],				// mounth
+												$date_start[3],				// day
+												$date_start[1]) ;			// year (4 digit)
+					$date_start_formater = date('d M Y',$date_start_time);
+					$heure_start = date('H:i',$date_start_time);
+					$date_start_formater = $jours_mini[date('w',$date_start_time)]." $date_start_formater";
+
+					$date_end_time = mktime(	isset($date_end[4])?$date_end[4]:0,	// hour
+												isset($date_end[5])?$date_end[5]:0,	// min
+												isset($date_end[6])?$date_end[6]:0,	// sec
+												$date_end[2],				// mounth
+												$date_end[3],				// day
+												$date_end[1]) ;			// year (4 digit)
+					$date_end_formater = date('d M Y',$date_end_time);
+					$heure_end = date('H:i',$date_end_time);
+					$date_end_formater = $jours_mini[date('w',$date_end_time)]." $date_end_formater";
+?>
+					<div class="cal-event" style="background:url(gfx/calendar.gif) no-repeat 4px top;margin-bottom:20px;float:left;width:20%;padding-left:26px;border-right:solid 1px #CCC;border-bottom:solid 1px #CCC;margin-right:20px;height:100px;border-radius:10px;">
+						<div class="cal-date" style="font-weight:bold;">
+							<?=$date_start_formater?>
+							<?=$heure_start <> '00:00'?$heure_start:'' ?>
+							<img src="gfx/arrow.png" style="vertical-align:bottom;"/>
+							<?=$date_end_formater <> $date_start_formater ? $date_end_formater:''?>
+							<?=$heure_end <> '00:00'?$heure_end:'' ?>
+						</div>
+						<div class="cal-summary" style="font-weight:normal;">
+							<?=utf8_decode($event['SUMMARY'])?>
+						</div>
+						<div class="cal-description" style="color:grey;font-weight:normal;">
+							<?=isset($event['DESCRIPTION']) ? utf8_decode($event['DESCRIPTION']):''?>
+						</div>
+					</div>
+<?				}
+			} else {
+				?>Impossible de trouver le fichier json <em><?=$ini['files']['json_output']?></em><?
+			}
+		} else {
+			 ?>Impossible de trouver le fichier de configuration <em><?=$ini_filename?></em><?
+		}
+	?>
 </div>
-
 </body>
 </html>
--- trunk/scripts/requette_divers.sql
+++ trunk/scripts/requette_divers.sql
@@ -27,3 +27,14 @@ update AFAGESTCOM.ASTOFIP1 set STO21='DFT' where DEPOT='AFA' and DAYS(DATE(NOW()
 
 -- pour les articles créer depuis moins d'un an, on affecte la formule NVX (sur AFA)
 update AFAGESTCOM.ASTOFIP1 set STO21='NVX' where DEPOT='AFA' and DAYS(DATE(NOW())) - DAYS(DATE(CONCAT(STCSS,CONCAT(STCAA,CONCAT('-',CONCAT(STCMM,CONCAT('-',STCJJ))))))) <= 365
+
+-- affecte des gestionnaires de stock par défaut pour les articles sans gestionnaire en fonction de l'activité article AFA
+update AFAGESTCOM.ASTOFIP1 set STGES='CG' where STGES='' and DEPOT='AFA' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00A' or ACTIV='00M')
+update AFAGESTCOM.ASTOFIP1 set STGES='JM' where STGES='' and DEPOT='AFA' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00B' or ACTIV='00P')
+update AFAGESTCOM.ASTOFIP1 set STGES='BT' where STGES='' and DEPOT='AFA' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00D' or ACTIV='00E' or ACTIV='00F' or ACTIV='00H' or ACTIV='00X')
+update AFAGESTCOM.ASTOFIP1 set STGES='CK' where STGES='' and DEPOT='AFA' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00C' or ACTIV='00G' or ACTIV='00J' or ACTIV='00K' or ACTIV='00L' or ACTIV='00N' or ACTIV='00T' or ACTIV='00W')
+
+-- affecte des gestionnaires de stock par défaut pour les articles sans gestionnaire en fonction de l'activité article AFL
+update AFAGESTCOM.ASTOFIP1 set STGES='VLP'  where STGES='' and DEPOT='AFL' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00D' or ACTIV='00E' or ACTIV='00H' or ACTIV='00J' or ACTIV='00K' or ACTIV='00W' or ACTIV='00X')
+update AFAGESTCOM.ASTOFIP1 set STGES='JMLB' where STGES='' and DEPOT='AFL' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00A' or ACTIV='00C' or ACTIV='00F' or ACTIV='00L' or ACTIV='00M')
+update AFAGESTCOM.ASTOFIP1 set STGES='GL'   where STGES='' and DEPOT='AFL' and NOART in (select NOART from AFAGESTCOM.AARTICP1 where ACTIV='00B' or ACTIV='00G' or ACTIV='00N' or ACTIV='00P')
